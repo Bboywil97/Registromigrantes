d449b716eba9b4330ef2ea9820ea24c991be398d
yzyDRNX <edaniellriv@gmail.com>
2025-01-20 13:36:26 -0600

    sksksk

--- Registromigrantes/Registromigrantes/Data/AplicationDbContext.cs
+++ Registromigrantes/Registromigrantes/Data/AplicationDbContext.cs
@@ -1,6 +1,20 @@
-﻿namespace Registromigrantes.Data
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace Registromigrantes.Server.Data
 {
-    public class AplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+
+
+        // public DbSet<NombreClase> NombreTablaEnBaseDeDatos { get; set; }
     }
 }
