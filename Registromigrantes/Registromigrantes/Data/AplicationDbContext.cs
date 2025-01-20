7d0c49e21884a4568fa9c82a59fe418053b41769
Bboywil97 <131089907+Bboywil97@users.noreply.github.com>
2025-01-20 13:33:47 -0600

    Basedatos

--- Registromigrantes/Registromigrantes/Data/AplicationDbContext.cs
+++ Registromigrantes/Registromigrantes/Data/AplicationDbContext.cs
@@ -1,6 +1,29 @@
-﻿namespace Registromigrantes.Data
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Registromigrantes.Components.Pages;
+using FormularioShared = Registromigrantes.shared.Formulario; // Alias para evitar ambigüedad
+using LoginShared = Registromigrantes.shared.Login; // Alias para evitar ambigüedad
+
+namespace Registromigrantes.Data
 {
-    public class AplicationDbContext
+    public class AplicationDbContext : DbContext
     {
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<FormularioShared> Formulario { get; set; } // Usar alias para evitar ambigüedad
+        public DbSet<LoginShared> Login { get; set; } // Usar alias para evitar ambigüedad
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<FormularioShared>()
+                .HasKey(f => f.Id); // Definir la clave primaria para la entidad Formulario
+            modelBuilder.Entity<LoginShared>()
+                .HasKey(f => f.Id); // Definir la clave primaria para la entidad Login
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
+
