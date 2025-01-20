8b5e7408caeb4d0deea387b308be7840b83e3ff1
Bboywil97 <131089907+Bboywil97@users.noreply.github.com>
2025-01-20 11:26:24 -0600

    cambios

--- Registromigrantes.shared/Formulario.cs
+++ Registromigrantes.shared/Formulario.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,19 @@ namespace Registromigrantes.shared
 {
     internal class Formulario
     {
+        public int id { get; set; }
 
+        public string Nombre { get; set; }
+
+        public string Apellidos { get; set; }
+
+        public int Edad { get; set; }
+
+        public string OcupacionPrevia { get; set; }
+        public string OcupacionDeseada { get; set; }
+
+        public string LugarOrigen { get; set; }
+
+        public string Observaciones { get; set; }
     }
 }
