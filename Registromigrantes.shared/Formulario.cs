122e505bfae37eca97aa9dd090a4a7a234e5572b
yzyDRNX <edaniellriv@gmail.com>
2025-01-22 09:42:46 -0600

    Diseño terminado

--- Registromigrantes.shared/Formulario.cs
+++ Registromigrantes.shared/Formulario.cs
@@ -4,6 +4,7 @@ using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Registromigrantes.Server.Data;
 
 namespace Registromigrantes.shared
 {
@@ -18,5 +19,40 @@ namespace Registromigrantes.shared
         public string LugarOrigen { get; set; }
         public string Observaciones { get; set; }
         public DateTime FechaCreacion { get; set; }
+    }
+
+    public class FormularioService
+    {
+        public async Task<string> CrearFormulario(Formulario formulario)
+        {
+            try
+            {
+                using (var context = new AplicationDbContext())
+                {
+                    context.Formulario.Add(formulario);
+                    await context.SaveChangesAsync();
+                    return "Formulario creado exitosamente";
+                }
+            }
+            catch (Exception ex)
+            {
+                return $"Error al crear el formulario: {ex.Message}";
+            }
+        }
+        public async Task<List<Formulario>> ObtenerFormularios()
+        {
+            try
+            {
+                using (var context = new AplicationDbContext())
+                {
+                    return await context.Formulario.ToListAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                return new List<Formulario>();
+            }
+        }
+
     }
 }
